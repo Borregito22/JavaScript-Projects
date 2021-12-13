31cdb81b43a7587de780a98365bcdef6b5774580
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2021-12-12 21:41:01 -0700

    Added new files for JavaScript coding

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,41 @@
+var A = "Hello, World!"
+window.alert(A); //Makes a pop up box that says "Hello, World!"
+
+var X = "This string has an apostrophe (\') and quotation marks (\").";
+document.write(X); //Writes out text utilizing the \ to add marks
+
+document.write("<br>") //Inserts a line break
+
+document.write("\"We are living in a culture entirely hypnotized by the"
++ " illusion of time, in which the so-called present moment is felt as"
++ " nothing but an infintesimal hairline between an all-powerfully"
++ " causative past and an absorbingly important future. We have no"
++ " present. Our consciousness is almost completely preoccupied with"
++ " memory and expectation. We do not realize that there never was, is,"
++ " nor will be any other experience than present experience. We are"
++ " therefore out of touch with reality. We confuse the world as talked"
++ " about, described, and measured with the world which actually is. We"
++ " are sick with a fascination for the useful tools of names and numbers,"
++ " of symbols, signs, conceptions and ideas.\""
++ " -Alan Watts");
+
+document.write("<br>")
+
+var B = "Concatenated" + " String" //+ concatenates two strings
+document.write(B)
+
+document.write("<br>")
+
+var Family = "The Griffins", Dad = "Peter", Mom = "Lois",
+    Daughter = "Meg", Son1 = "Chris", Son2 = "Stewie", Pet = "Brian";
+var Family = Family.fontcolor("green");
+var Dad = Dad.fontcolor("violet");
+var Mom = Mom.fontcolor("yellow");
+var Daughter = Daughter.fontcolor("blue");
+var Son1 = Son1.fontcolor("green");
+var Son2 = Son2.fontcolor("red")
+document.write(Dad);
+
+document.write("<br>")
+
+document.write(420+69) //An expression
