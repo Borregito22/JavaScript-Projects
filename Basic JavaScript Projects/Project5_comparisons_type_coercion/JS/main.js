cca1f2222b7cf102906b5a15908802ea9ec3d7ef
Borregito22 <93361779+Borregito22@users.noreply.github.com>
2021-12-14 16:05:14 -0700

    Added 2 new files for type coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,49 @@
+// "Type Of" Operator
+document.write(typeof true, "<br>");
+
+// Type Coercion
+document.write(12 + 4 + "six", "<br>");
+
+// Infinity and -Infinity
+document.write(9E400, "<br>");
+document.write(-7E310, "<br>");
+
+// Boolean Logic
+document.write(3850 > 2530, "<br>");
+document.write(298 > 2342, "<br>");
+
+// Console.log Function
+console.log(26 * 36,);
+
+// == Function
+document.write(935 == 935, "<br>");
+document.write(346 == 643, "<br>"); 
+
+// === Function
+A = 4358
+B = 4358
+document.write(A === B, "<br>"); 
+
+X = 834
+Y = "Beans"
+document.write(X === Y, "<br>"); 
+
+C = 1375
+D = "1375"
+document.write(C === D, "<br>"); 
+
+E = 6327
+F = 7413
+document.write(E === F, "<br>");
+
+// AND Operator
+document.write(6348 > 3459 && 2359 > 1035, "<br>");
+document.write(3459 > 3459 && 2359 > 1035, "<br>");
+
+// OR Operator
+document.write(4304 > 2340 || 1629 > 7241, "<br>");
+document.write(6172 > 8013 || 3794 > 8037, "<br>");
+
+// NOT Operator
+document.write(!(1987 > 3054), "<br>");
+document.write(!(539 > 502));
